0508bdd5a376bf412d206229ab7366753fb8b30c
Milana.Avtukh <Milana.Avtukh@x5.ru>
2023-03-21 15:23:02 +0300

    add db session

---
+++ tests/db_session.py
@@ -0,0 +1,14 @@
+from fastapi import Depends
+from fastapi.params import Depends as DependsType
+from sqlalchemy.orm import Session
+
+from src.dependencies import get_db
+
+
+class SessionMixin:
+    __session: Session = Depends(get_db())
+
+    def session(self) -> Session:
+        if isinstance(self.__session, DependsType):
+            return next(get_db())
+        return self.__session
--- tests/test_authors.py
+++ tests/test_authors.py
@@ -1,30 +1,37 @@
-from src.authors.router import (create_author, delete_author, get_author_by_id,
-                                get_authors)
+from src.authors.router import (
+    create_author,
+    delete_author,
+    get_author_by_id,
+    get_authors,
+)
 from src.database.schemas import AuthorCreate
-from src.dependencies import get_db
+
+from .db_session import SessionMixin
 
 
 def test_create_author(snapshot):
     snapshot.snapshot_dir = "snapshots/authors"
-    value = create_author(
-        AuthorCreate(**dict(id=99, name="Shakespeare")), db=next(get_db())
-    )
+    session = SessionMixin().session()
+    value = create_author(AuthorCreate(**dict(id=99, name="Shakespeare")), db=session)
     snapshot.assert_match(value.body, "create_author.json")
 
 
 def test_get_authors(snapshot):
     snapshot.snapshot_dir = "snapshots/authors"
-    values = get_authors(db=next(get_db()))
+    session = SessionMixin().session()
+    values = get_authors(db=session)
     snapshot.assert_match(values.body, "get_authors.json")
 
 
 def test_get_author_by_id(snapshot):
     snapshot.snapshot_dir = "snapshots/authors"
-    value = get_author_by_id(99, db=next(get_db()))
+    session = SessionMixin().session()
+    value = get_author_by_id(99, db=session)
     snapshot.assert_match(value.body, "get_author_by_id.json")
 
 
 def test_delete_author(snapshot):
     snapshot.snapshot_dir = "snapshots/authors"
-    value = delete_author(99, db=next(get_db()))
+    session = SessionMixin().session()
+    value = delete_author(99, db=session)
     snapshot.assert_match(value.body, "delete_author.json")
